4bbd10ba20c353819fc32f058e4ad63efb47e22c
Islam <alnagah572@gmail.com>
2026-07-26 19:52:26 +0300

    Configure customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    protected $fillable = [
+        'full_name',
+        'phone',
+        'email',
+        'city',
+        'country',
+        'service',
+        'source',
+        'price',
+        'status',
+        'notes',
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+    ];
 }
